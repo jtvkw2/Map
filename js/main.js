ea1c6096ade3d8dac6de751383b1f348c1073bb7
Jacob Voyles <jtvkw2@gmail.com>
2018-01-14 01:41:51 -0800

    InfoWindow now displays foursquare information

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  //  document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
 
-// Data for the markers consisting of a name, a LatLng and a zIndex for the
-// order in which these markers should display on top of each other.
 var places = [
-  ['Fort Lewis', 47.1261838830712, -122.50854491937491, 5],
-  ['NAS Whidbey Island', 48.34709388383968, -122.66647338617179, 4],
-  ['Bangor Base', 47.72035804907695, -122.70973205316398, 3],
-  ['Keyport Base', 47.697255902139425, -122.62184142816398, 2],
-  ['Naval Base Kitsap', 47.56028140643856, -122.64587402093741, 1]
+  ['Scenic Beach State Park', 47.649061, -122.845790, 5],
+  ['Kitsap Memorial State Park', 47.817918, -122.650993, 4],
+  ['Illahee State Park', 47.595597, -122.596520, 3],
+  ['Blake Island Marine State Park', 47.538398, -122.492881, 2],
+  ['Dosewallips State Park', 47.688125, -122.904649, 1]
 ];
-
 
 var map;
 var infowindow;
@@ -28,7 +23,7 @@ function initMap() {
     var latlng = {lat: 47.6410663, lng: -122.6881319};
     var mapOptions = {
         center: latlng,
-        zoom: 8
+        zoom: 10
     };
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
     infowindow = new google.maps.InfoWindow();
@@ -38,38 +33,46 @@ function initMap() {
 }
 
 function addMarkers(newPlace) {
+  var ltln = newPlace[1] + "," + newPlace[2];
+  var client_id = "&client_id=QF1NFRPXGG5WABAHQMTFQLZBXY3UTUIGU3AOAL0UP1X5VCOO"
+  var client_secret = "&client_secret=EUFMT3BWBSYYSSL5U52JLKWBVEJWZNY5CZXCB3RQAFRMEZV3";
+  var venueAPI = "https://api.foursquare.com/v2/venues/search?ll="+ltln+"&v=20140715"+client_id+client_secret;
+  var currentNum, currentURL, currentDescription;
+  $.getJSON(venueAPI, function(data) {
+                console.log(data);
+                currentURL = data.response.venues[0].url;
+                currentNum = data.response.venues[0].contact.formattedPhone;
+                currentDescription = data.response.venues[0].location.address;
+  });
     var marker = new google.maps.Marker({
         position:{lat: newPlace[1], lng: newPlace[2]},
         map: map
     });
     marker.addListener('click', function() {
        marker.setAnimation(google.maps.Animation.DROP);
-       infowindow.setContent('<h3 class = "infoTitle"> '+newPlace[0]+'</h3>'+ '<div id="names">'+content+
-       '</div>');
+       infowindow.setContent('<h3 class = "infoTitle"> '+newPlace[0]+'</h3>'+ '<div id="names">'+ currentNum+ '</div>' +'<div>'+currentURL+'</div>'+'<div>'+currentDescription+'</div>');
        infowindow.open(map, this);
     });
 }
 
-
-
-
 var locations = [
   {
-  name: "Fort Lewis",
+  name: "Scenic Beach State Park",
   index: 0},
 {
-  name: "NAS Whidbey Island",
+  name: "Kitsap Memorial State Park",
   index: 1},
 {
-  name: "Bangor Base",
+  name: "Illahee State Park",
   index: 2},
 {
-  name: "Keyport Base",
+  name: "Blake Island Marine State Park",
   index: 3},
 {
-  name: "Naval Base Kitsap",
+  name: "Dosewallips State Park",
   index: 4}
 ];
+
 /* Sortable Lists */
 $(function() {
 
@@ -88,7 +91,7 @@ var viewModel = {
       });
     }
 
-//});
+
 };
 
 viewModel.locations = ko.dependentObservable(function() {
@@ -100,8 +103,8 @@ viewModel.locations = ko.dependentObservable(function() {
 
 ko.applyBindings(viewModel);
 });
-/*
 
+/*
 const request = require('request');
 
 request({
